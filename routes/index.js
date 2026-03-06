652bbf558792f27d7946aadcfe630e61c6523823
xx <zarieeeeel@gmail.com>
2026-03-06 14:15:51 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 //Route to get all users
-router.get("/enrollment", userController.getAllEnroll);
+router.get("/enrollments", userController.getAllEnroll);
 
 //router.get('/users/:,ip_address',userController.getUserByIp);
-router.post("/enrollment", userController.createEnroll);
+router.post("/enrollments", userController.createEnroll);
 
 module.exports = router;
+
