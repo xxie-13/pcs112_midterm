a354169fa5d19a974e92841c0b448f72d7da20a7
xx <zarieeeeel@gmail.com>
2026-03-09 15:34:00 +0800

    Fix typo in userController methods for enrollments

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 //Route to get all users
-router.get("/enrollments", userController.getAllEnroll);
+router.get("/enrollments", userController.getAllEnrollment);
 
 //router.get('/users/:,ip_address',userController.getUserByIp);
-router.post("/enrollments", userController.createEnroll);
+router.post("/enrollments", userController.createEnrollment);
 
 module.exports = router;
+
 
